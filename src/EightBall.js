d777817bb41f3043898804d79b77ec8160ee7321
Binyameen <bmghafoor@gmail.com>
2023-05-21 14:08:43 -0400

    update color on click and added reset button

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,7 +4,7 @@ import answers from "./answers";
 
 const EightBall = () => {
   const [ballText, setballText] = useState("Think of a question");
-  const [ballClass, setballClass] = useState("blackBall");
+  const [ballColor, setballColor] = useState("black");
 
   function randomItem(arr) {
     const randomNum = Math.floor(Math.random() * arr.length);
@@ -14,14 +14,26 @@ const EightBall = () => {
   function updateBall() {
     let randomAnswer = randomItem(answers);
     let answer = randomAnswer.msg;
+    let color = randomAnswer.color;
     setballText(answer);
+    setballColor(color);
+  }
+
+  function restart() {
+    setballText('Think of a question')
+    setballColor('black')
   }
 
   return (
     <>
-      <div onClick={updateBall} className={ballClass}>
+      <div
+        onClick={updateBall}
+        className="ball"
+        style={{ background: ballColor }}
+      >
         <span className="question">{ballText}</span>
       </div>
+      <button onClick = {restart}>Restart</button>
     </>
   );
 };
