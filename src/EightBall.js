1615ea75194b5581a898c27ce7dde35e54e6ed5a
Binyameen <bmghafoor@gmail.com>
2023-05-21 14:01:56 -0400

    updates answer on click

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
-import "./EightBall.css"
+import "./EightBall.css";
+import answers from "./answers";
 
 const EightBall = () => {
-    const [ballText, setballText] = useState('Think of a question')
+  const [ballText, setballText] = useState("Think of a question");
+  const [ballClass, setballClass] = useState("blackBall");
+
+  function randomItem(arr) {
+    const randomNum = Math.floor(Math.random() * arr.length);
+    return arr[randomNum];
+  }
+
+  function updateBall() {
+    let randomAnswer = randomItem(answers);
+    let answer = randomAnswer.msg;
+    setballText(answer);
+  }
+
   return (
     <>
-      <div className="blackBall">
+      <div onClick={updateBall} className={ballClass}>
         <span className="question">{ballText}</span>
       </div>
     </>
